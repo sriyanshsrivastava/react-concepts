c4d5de59fdb251ae4fbf53e500864465551d0740
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-09-09 02:19:21 +0530

    small style changes

--- 03-password-generator/src/App.jsx
+++ 03-password-generator/src/App.jsx
@@ -36,11 +36,11 @@ function App() {
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg p-4 my-8 text-orange-500 bg-gray-800 h-fit'>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg p-4 my-8 text-orange-500 bg-gray-800 h-fit duration-200'>
         <h1 className='text-white text-center my-3 text-xl font-bold'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input type="text" value={password} className='outline-none w-full py-1 px-3 bg-amber-100 font-bold' placeholder='Password' readOnly ref={passwordRef} />
-          <button onClick= {copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer font-bold uppercase'>Copy</button>
+          <button onClick= {copyPasswordToClipboard} className='outline-none bg-blue-500 text-white px-3 py-0.5 shrink-0 cursor-pointer font-bold uppercase hover:bg-blue-700 active:bg-blue-900 transition-all duration-200'>Copy</button>
         </div>
         <div className='flex text-sm gap-x-4 justify-center'>
           <div className='flex items-center gap-x-2'>
